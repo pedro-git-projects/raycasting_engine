bb57b5d06e3735cce54df08ab706917d8dbdffe8
pedro-git-projects <pedro.coding.contact@gmail.com>
2021-12-04 12:20:31 -0300

    created grid atribute

---
+++ js/p5.js
@@ -0,0 +1,11 @@
+function setup() {
+    // TODO: Initialize all objects
+}
+
+function update() {
+    // TODO: update all game objects before we render the next frame
+}
+
+function draw() {
+    // TODO: render all objects frame by frame
+}
---
+++ js/raycast.js
@@ -0,0 +1,33 @@
+const TILE_SIZE = 32;
+const MAP_NUM_ROWS = 11;
+const MAP_NUM_COLS = 15; 
+
+class Map {
+    constructor() {
+        this.grid = [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+            [1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        ];
+    }
+}
+
+function setup() {
+    // TODO: Initialize all objects
+}
+
+function update() {
+    // TODO: update all game objects before we render the next frame
+}
+
+function draw() {
+    // TODO: render all objects frame by frame
+}
